238af9cee76c98c310ef725fe87f3ba7742db04b
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-07-13 15:25:48 +0300

    add section folders

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./LandingPage.css"
 import HeroContainer from '../../components/landingpagecomponents/herocontainer/HeroContainer'
+import RecentPosts from '../../components/landingpagecomponents/recentposts/RecentPosts'
+import FeauteredWorks from '../../components/landingpagecomponents/feauteredworks/FeauteredWorks'
 export default function LandingPage() {
   return (
     <div className="landingPage">
       <HeroContainer/>
+      <RecentPosts/>
+      <FeauteredWorks/>
     </div>
   )
 }
