67d3ccd5e8e2e0838e209d3259b0f6bc0f866196
AbdiIbrahimAdan <abdishu98@gmail.com>
2024-07-13 13:21:59 +0300

    completed the Navbar component

--- src/components/navbarcomponents/Navbar.jsx
+++ src/components/navbarcomponents/Navbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaBars } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
+import { Link, useLocation } from 'react-router-dom';
+import './Navbar.css'
 
-export default function Navbar() {
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const location = useLocation();
   return (
-    <div>Navbar</div>
+    <>
+      <section className='Navbar-section'>
+        <nav className='main'>
+        <ul className={click ? "mobile-nav" : "main-nav"} onClick={() => setClick(false)}>
+          <li><Link to= '/' className={location.pathname === '/' ? 'active' : ''}></Link></li>
+          <li><Link to='/blogs' className={location.pathname === '/blogs' ? 'active' : ''}>Blogs</Link></li>
+          <li><Link to='/work'  className={location.pathname === '/work' ? 'active' : ''}>Works</Link></li>
+          <li><Link to='/contact'  className={location.pathname === '/contact' ? 'active' : ''}>Contact</Link></li>
+        </ul>
+        <button className='toggle' onClick={() => setClick(!click)}>           
+          {click ?  <FaTimes /> :  <FaBars />}
+           </button>
+        </nav>
+      </section>
+    </>
   )
 }
+
+export default Navbar
