6cf099f02eeb8e84cbed82a4b024691e1817ed9b
Gakiri001 <gakirijulius@gmail.com>
2024-07-14 14:44:48 +0300

    Unit testing for the blog page

---
+++ src/Data/Blogspage.js
@@ -0,0 +1,20 @@
+const blogs = [
+  {
+    header: "UI Interactions of the week",
+    paragraph: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit official consequat duis enim mollit. Exercitation veniam consequant sunt nostrud amet",
+  },
+  {
+    header: "UI Interactions of the week",
+    paragraph: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit official consequat duis enim mollit. Exercitation veniam consequant sunt nostrud amet",
+  },
+  {
+    header: "UI Interactions of the week",
+    paragraph: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit official consequat duis enim mollit. Exercitation veniam consequant sunt nostrud amet",
+  },
+  {
+    header: "UI Interactions of the week",
+    paragraph: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit official consequat duis enim mollit. Exercitation veniam consequant sunt nostrud amet",
+  },
+];
+
+export default blogs
