f71c292b7cfd683d21eb3754629399b01202081b
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-07-15 15:21:48 +0300

    remove dead code an fix typos

--- src/components/landingpagecomponents/feauteredworks/Feauture.test.jsx
+++ src/components/landingpagecomponents/feauteredworks/Feauture.test.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import FeauteredWorks from './FeauteredWorks';
-import { works } from '../../../data/landingpagedata/WorkData';
-import WorkCard from '../../workcardcomponent/WorkCard';
 
 describe('FeauteredWorks Component', () => {
-  it('renders the FeauteredWorks component', () => {
+  it('renders the FeauteredWorks component title', () => {
     render(<FeauteredWorks />);
     const featureText=screen.getByText(/FeauteredWorks/i)
     expect(featureText).toBeInTheDocument();
