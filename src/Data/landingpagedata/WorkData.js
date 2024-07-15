e8515524bda4cabebb1ffb6f3085ad30c8ca12ec
Jasper Ngunjiri Wambugu <jasperwambugu@gmail.com>
2024-07-15 14:36:18 +0300

    add reusable component for the work page

---
+++ src/Data/landingpagedata/WorkData.js
@@ -0,0 +1,8 @@
+import { work1, work2, work3, work4 } from "../../assets/images";
+
+export const works=[
+    {picture:work1, tagtitle:"Designing Dashboards", year:"2020", subtitle:"Dashboard", message:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt accusantium exercitationem recusandae corporis iusto, error excepturi distinctio impedit, iste illo ex sapiente cumque officia, reprehenderit pariatur."},
+    {picture:work2, tagtitle:"vibrant potraits of 2020", year:"2018", subtitle:"Illustration", message:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt accusantium exercitationem recusandae corporis iusto, error excepturi distinctio impedit, iste illo ex sapiente cumque officia, reprehenderit pariatur."},
+    {picture:work3, tagtitle:"36 days of malayalam type", year:"2018", subtitle:"Typography", message:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt accusantium exercitationem recusandae corporis iusto, error excepturi distinctio impedit, iste illo ex sapiente cumque officia, reprehenderit pariatur."},
+    {picture:work4, tagtitle:"Components", year:"2020", subtitle:"Components, Design", message:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt accusantium exercitationem recusandae corporis iusto, error excepturi distinctio impedit, iste illo ex sapiente cumque officia, reprehenderit pariatur."},
+]
